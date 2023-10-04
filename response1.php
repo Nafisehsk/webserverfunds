83c5b3e99921d6ef9d3e615d685436d20d39245d
Nafisehsk <nafiseh.sarhang@gmail.com>
2023-10-04 13:38:40 -0400

    lab5formphp

--- response1.php
+++ response1.php
@@ -28,13 +28,34 @@
             font-family: Arial, Helvetica, sans-serif;
             font-size: 20px;
             }
+        
         </style>
+
+            if (in_array('answer1', $_POST))
+                echo "You have selected the first option for question 2"
+            if (in_array('answer2', $_POST)) echo "You have selected the second option for question 2"
+            if ((in_array('answer1', $_POST)) && (in_array('answer2', $_POST)) )
+                echo "You have selected both options for question 2"
+            else echo "You have selected no option for question 2"
+            
+
     </head>
 
     <body>
         
-        <p><?= var_dump($_GET) ?></p>
-        <p><?= var_dump($_POST) ?></p>
+        <p>Hello <?= htmlspecialchars($_POST ['fname']); . htmlspecialchars($_POST ['lname']);?></p>
+        <p>Your are born on: <?= (int)$_POST ['bd']); ?></p>
+        <p>You have selected <?= $_POST ['response'] ?></p>
+        <?php if (in_array('answer1', $_POST)): ?>
+            <p>echo "You have selected the first option for question 2."</p>
+        <?php if (in_array('answer2', $_POST)): ?>
+            <p>echo "You have selected the second option for question 2"</p>
+        <?php else if ((in_array('answer1', $_POST)) && (in_array('answer2', $_POST))): ?>
+            <p>echo "You have selected both options for question 2"</p>
+        <?php else: ?>
+            <p>else echo "You have selected no option for question 2"</p>
+        <?php endif; ?>
+             
 
         
     </body>
