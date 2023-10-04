4f2b13883ca794a80c0905347d2297e40383decd
Nafisehsk <nafiseh.sarhang@gmail.com>
2023-10-04 14:13:21 -0400

    Update response1.php

--- response1.php
+++ response1.php
@@ -39,15 +39,15 @@
         <p>Hello <?= htmlspecialchars($_POST ['fname']) . " " . htmlspecialchars($_POST ['lname'])?>!</p>
         <p>You are born on: <?=$_POST ['bd'] ?></p>
         <p>You have selected <?= $_POST ['response'] ?> for question 1.</p>
-        <?php if ((in_array('answer1', $_POST)) && (in_array('answer2', $_POST))) {?> 
+        <?php if ((in_array("answer1", $_POST)) && (in_array("answer2", $_POST))) ?> 
             <p>You have selected both options for question 2.</p>
-        <?php } elseif (in_array('answer2', $_POST)) {?>
+        <?php  elseif (in_array("answer2", $_POST)) ?>
             <p>You have selected the second option for question 2.</p>
-        <?php } elseif (in_array('answer1', $_POST)) {?>
+        <?php  elseif (in_array("answer1", $_POST)) ?>
             <p>You have selected the first option for question 2.</p>
-        <?php } else { ?>
+        <?php  else ?>
             <p>You have not selected any options for question 2.</p>
-        <?php } ?>
+        <?php  ?>
                
     </body>
 
