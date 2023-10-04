f1c4b222a48db7cbe1bf4d6b7c7b3b331e8d17b5
Nafisehsk <nafiseh.sarhang@gmail.com>
2023-10-04 12:47:41 -0400

    Update response1.php

--- response1.php
+++ response1.php
@@ -1,6 +1,34 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <style>
+            body {
+            background-color: rgb(19, 56, 68);
+            color: rgb(228, 240, 236);
+            font-family: Arial, Helvetica, sans-serif;
+            }
+
+            text {
+            font-size: 9px;
+            }
+            
+
+            h1 {
+            color: white;
+            text-align: left;
+            }
+
+            h5 {
+            color: red;
+            text-align: left;
+            }
+            
+            p {
+            color: rgb(143, 202, 219);
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 20px;
+            }
+        </style>
     </head>
 
     <body>
