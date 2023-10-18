e1a02212413c578923877beef3a2b6bcf77eb1d1
Nafisehsk <nafiseh.sarhang@gmail.com>
2023-10-18 14:52:05 -0400

    Update form2.php

--- form2.php
+++ form2.php
@@ -86,7 +86,7 @@
 
                 foreach($result as $row) 
                 {
-                    echo "<option value='{$row['course_number']}'>{$row['course_number']}</option>\n";
+                    echo "<option value='{$row['course_name']}'>{$row['course_number']}</option>\n";
                 }
                 // Don't forget to close the connection!
                 mysqli_close($conn);
